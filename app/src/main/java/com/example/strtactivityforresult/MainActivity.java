3f8214e71fecb35af872234e02407c35723a68b0
davidnardya@gmail.com <`1q`1q~!Q>
2020-03-14 18:12:10 +0200

    Dev branch Added GSON in MainActivity

--- app/src/main/java/com/example/strtactivityforresult/MainActivity.java
+++ app/src/main/java/com/example/strtactivityforresult/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.strtactivityforresult;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,18 +13,28 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.strtactivityforresult.pojo.Product;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     final int REQUEST_CODE = 12345;
 
     Button selectProductsBtn;
+    DataManager dataManager = new DataManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         selectProductsBtn = findViewById(R.id.select_product_btn);
+
+        RecyclerView selectedProductsList = findViewById(R.id.main_activity_container);
+        selectedProductsList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL,false));
+        final MainAdapter adapter = new MainAdapter(dataManager.selectedProduct);
+
+        selectedProductsList.setAdapter(adapter);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -37,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }, 5000);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            if(data != null){
+                String productString = data.getStringExtra("list");
+                Gson gson = new Gson();
+                Product product = gson.fromJson(productString, Product.class);
+                DataManager.setSelectedProduct();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/strtactivityforresult/MainAdapter.java
+++ app/src/main/java/com/example/strtactivityforresult/MainAdapter.java
@@ -1,7 +1,9 @@
 package com.example.strtactivityforresult;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,8 +15,8 @@ import java.util.ArrayList;
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
 
     DataManager dataManager = new DataManager();
-    
-    ArrayList<Product> selectedProductsList = new ArrayList<>();
+
+    ArrayList<Product> selectedProductsList;
 
     public MainAdapter(ArrayList<Product> selectedProductsList) {
         this.selectedProductsList = selectedProductsList;
@@ -23,26 +25,32 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder
     @NonNull
     @Override
     public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_product_cell,parent,false);
+
+        return new MainViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
-
+        Product item = selectedProductsList.get(position);
+        holder.productName.setText(item.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return selectedProductsList.size();
     }
 
     public class MainViewHolder extends RecyclerView.ViewHolder{
 
         RecyclerView recyclerView;
+        TextView productName;
 
         public MainViewHolder(@NonNull View itemView) {
             super(itemView);
             recyclerView = itemView.findViewById(R.id.main_activity_container);
+            productName = itemView.findViewById((R.id.product_name_tv));
         }
     }
 }
